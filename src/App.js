276d979b376fbb1702765f33479320670a99383c
Kevin Forrest Connors <kevinforrestconnors@gmail.com>
2021-12-24 14:54:54 -0500

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,7 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+
+import { WalletModalButton } from "@solana/wallet-adapter-react-ui";
 
 function App() {
   return (
